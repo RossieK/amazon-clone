562ea84534a9c9301ab6fcfca2954c3fdcc17808
RossieK <rosi.kirova96@gmail.com>
2021-05-30 19:22:44 +0300

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
+import "./styles/App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello world!</h1>
+      <Header />
     </div>
   );
 }
