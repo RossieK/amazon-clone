5a2413f071e363021d0bd84c0cae9ef8d5523337
RossieK <rosi.kirova96@gmail.com>
2021-06-03 19:37:10 +0300

    Add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./styles/App.css";
+import {Switch, Route} from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
+      <Switch>
+        <Route path="/" exact>
+          <Header />
+          <Home />
+        </Route>
+        <Route path="/checkout" exact>
+          <Header />
+          <h1>Checkout page</h1>
+        </Route>
+      </Switch>
     </div>
   );
 }
