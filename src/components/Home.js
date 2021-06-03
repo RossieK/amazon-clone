198f40b248c3eb7ffcbfe30c3c89ecd58ef784b8
RossieK <rosi.kirova96@gmail.com>
2021-06-03 19:30:02 +0300

    Add props to Product and some mock data

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,16 +11,46 @@ function Home() {
           alt="Amazon background"
         />
         <div className="home__row">
-          <Product />
-          <Product />
+          <Product
+            title="The lean startup"
+            price={19.99}
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx3BIWym5q5kW-7rNV4gQUTzLiYLLH0FGUADd_iM9u6oRVIY18gxxgt5WJLixXYcHctkc&usqp=CAU"
+            rating={3}
+          />
+          <Product
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5L Glass Bowl, Removable Splash Guard, 1000 W, Cream"
+            price={299.99}
+            image="https://m.media-amazon.com/images/I/51LH9DtmipL._AC_UY218_.jpg"
+            rating={5}
+          />
         </div>
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            title="Panasonic LUMIX DC-S5 S5 Full Frame Mirrorless Camera body, 4K 60P Video Recording with Flip Screen and Wi-Fi, L-Mount, 20-60 mm Lens, 5-Axis Dual I.S, (Black)"
+            price={42.29}
+            image="https://m.media-amazon.com/images/I/81rMhM-GLAL._AC_SX180_SY120_QL70_.jpg"
+            rating={4}
+          />
+          <Product
+            title="EEFINITE Fitness Tracker with Alexa Built-in, 5ATM Waterproof Activity Tacker with Blood Oxygen Heart Rate Monitor, Sleep & Swim Tracking, Smart Fitness Band Step Calorie Counter for Women Men"
+            price={34.99}
+            image="https://m.media-amazon.com/images/I/61UFB9nPFLL._AC_UY218_.jpg"
+            rating={4}
+          />
+          <Product
+            title="Smart GU10 LED Bulbs, Warm White to Cold White Dimmable, RGB Multi Colour Changing WiFi MR16 Spotlight Bulb, Compatible with Alexa & Google Home, 5W=50W, 2.4Ghz Only, Pack of 4 by Torkase"
+            price={39.99}
+            image="https://m.media-amazon.com/images/I/51LZ+nwuW-S._AC_UY218_.jpg"
+            rating={5}
+          />
         </div>
         <div className="home__row">
-          <Product />
+          <Product
+            title="Philips Ambilight 55OLED805/12 55-Inch OLED TV (4K UHD, P5 AI Perfect Picture Engine, Dolby Vision, Dolby Atmos, HDR 10+, Freeview Play, Works with Alexa, Android TV) Gun Metal Grey (2020/2021 Model)"
+            price={1149.99}
+            image="https://m.media-amazon.com/images/I/61g4UK2QwWL._AC_UY218_.jpg"
+            rating={4}
+          />
         </div>
       </div>
     </div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,21 +1,23 @@
 import "../styles/Product.css";
 
-function Product() {
+function Product({id, title, image, price, rating}) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__raiting">
-          <p>⭐️</p>
-          <p>⭐️</p>
-          <p>⭐️</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐️</p>
+            ))}
         </div>
       </div>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx3BIWym5q5kW-7rNV4gQUTzLiYLLH0FGUADd_iM9u6oRVIY18gxxgt5WJLixXYcHctkc&usqp=CAU" alt="Book cover" />
+      <img src={image} alt="{title}" />
       <button>Add to Basket</button>
     </div>
   );
