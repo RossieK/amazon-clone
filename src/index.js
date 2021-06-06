86586a159abd20d83391e1eca4760b483f75189a
RossieK <rosi.kirova96@gmail.com>
2021-06-06 12:35:39 +0300

    Setup context API

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./App";
 import {BrowserRouter} from "react-router-dom";
+import {StateProvider} from "./StateProvider";
+import {initialState, reducer} from "./reducer";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StateProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
