beeeb7b4bd440ef101ab9a6539ad4a7d27bf3cc2
RossieK <rosi.kirova96@gmail.com>
2021-06-06 14:04:19 +0300

    Add option to remove item from basket

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,6 +1,13 @@
 import "../styles/CheckoutProduct.css";
+import {useStateValue} from "../StateProvider";
 
 function CheckoutProduct({id, image, title, price, rating}) {
+  const [{basket}, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    dispatch({type: "REMOVE_FROM_BASKET", id: id});
+  };
+
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} alt={title} />
@@ -17,7 +24,7 @@ function CheckoutProduct({id, image, title, price, rating}) {
               <p key={i}>⭐️</p>
             ))}
         </div>
-        <button>Remove from basket</button>
+        <button onClick={removeFromBasket}>Remove from basket</button>
       </div>
     </div>
   );
